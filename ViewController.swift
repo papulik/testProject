aa4b4dfa76c02ccf1db2e2289d615ce27d1e017d
zuka papuashvili <zurabpapuashvili@gmail.com>
2023-01-18 02:43:56 +0400

    initial commit

---
+++ ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TestProject
+//
+//  Created by Zuka Papuashvili on 18.01.23.
+//
+
+import UIKit
+
+
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var label: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        label.text = "Hello World"
+    }
+
+
+}
+
